ed094b89e96558517c84c9352bc09b89204db408
Peter Li <lihaohong6@gmail.com>
2024-10-29 17:26:58 -0400

    make upload script more generic

--- upload.py
+++ upload.py
@@ -12,15 +12,16 @@ s: APISite = pwb.Site()
 s.login()
 path = Path("./upload")
 
+
 def upload_cut_scenes():
     image_path = Path(r"./upload")
-    
+
     def glob() -> list[Path]:
         def name_filter(f: str) -> bool:
             return re.search(r"_(Kr|kr|Tw|tw|Th|th)\.jpg", f) is None
-            
+
         return [f for f in image_path.glob("*.jpg") if name_filter(f.name)]
-    
+
     already_exist = set()
     gen = (FilePage(s, "File:" + f.name) for f in glob())
     preload = PreloadingGenerator(generator=gen)
@@ -34,41 +35,42 @@ def upload_cut_scenes():
         is_cut_scene = False
         if f.name.startswith("BG_CS"):
             is_cut_scene = True
-            
+
         if is_cut_scene:
             cat = "[[Category:Cutscenes]]"
         else:
             cat = "[[Category:Background images]]"
-        
+
         try:
-            s.upload(FilePage(s, "File:" + f.name), source_filename=str(f), comment="Batch upload background images and cutscenes",
+            s.upload(FilePage(s, "File:" + f.name), source_filename=str(f),
+                     comment="Batch upload background images and cutscenes",
                      text=cat)
         except Exception as e:
             print(f.name, e)
 
-def upload_files():
-    EXTENSIONS = ["png", "jpg"]
-    COMMENT = "Batch upload aoharu images"
-    TEXT = "[[Category:4-panel manga]]"
-    REDIRECT = False
+
+def upload_files(extensions: tuple[str, ...],
+                 text: str,
+                 comment: str,
+                 redirect: bool):
     already_exist = set()
-    
+
     def name_mapper(original: str, with_file: bool = True) -> str:
         prefix = ""
         if with_file:
             prefix = "File:"
         return prefix + original
         return prefix + "Memorial Lobby " + original
-    
+
     def get_all_files():
         result = []
-        for e in EXTENSIONS:
+        for e in extensions:
             result.extend(path.glob(f"*.{e}"))
         return result
-            
+
     file_list = get_all_files()
     print(f"{len(file_list)} files found")
-    
+
     gen = (FilePage(s, name_mapper(f.name)) for f in file_list)
     preload = PreloadingGenerator(generator=gen)
     exists_count = 0
@@ -83,13 +85,13 @@ def upload_files():
         if name_mapper(f.name, with_file=False) in already_exist:
             continue
         try:
-            s.upload(FilePage(s, name_mapper(f.name)), source_filename=str(f), comment=COMMENT,
-                     text=TEXT)
+            s.upload(FilePage(s, name_mapper(f.name)), source_filename=str(f), comment=comment,
+                     text=text)
         except Exception as e:
-            if not REDIRECT:
+            if not redirect:
                 continue
             error_string = str(e)
-            search = re.search(r"duplicate of", error_string)
+            search = re.search(r"duplicate of \['([^']+)'", error_string)
             if search is not None:
                 p = FilePage(s, name_mapper(f.name))
                 p.text = f"#REDIRECT [[File:{search.group(1)}]]"
@@ -97,11 +99,19 @@ def upload_files():
             else:
                 print(f.name, "\n", e)
 
+
+def upload_bgm():
+    upload_files(("ogg",),
+                 text="[[Category:Background music]]",
+                 comment="Batch upload bgm",
+                 redirect=True)
+
+
 def rename_files():
     files = path.glob("*.png")
     for f in files:
         fname = f.name
         f.rename(path.joinpath(f"Fankit {fname}"))
-        
-        
-upload_cut_scenes()
+
+
+upload_bgm()
