19ce10706fc0d33c6439523c9cfd0beac1105d5b
Peter Li <lihaohong6@gmail.com>
2024-10-29 21:29:06 -0400

    revive sprite face identification script

--- portrait_spirite.py
+++ portrait_spirite.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 import subprocess
 import requests
@@ -12,32 +13,40 @@ sys.path.insert(1, str(dino_path.absolute()))
 source_path = Path("../ba-spinecharacters")
 
 
-# def download():
-#     from pywikibot import FilePage, Site
-#     from pywikibot.pagegenerators import GeneratorFactory
+def download():
+
+    from pywikibot import FilePage, Site
+    from pywikibot.pagegenerators import GeneratorFactory
+
+    s = Site()
     
-#     def download_file(url):
-#         from urllib.parse import unquote
-#         local_filename = path.joinpath(unquote(url.split('/')[-1]))
-#         # NOTE the stream=True parameter below
-#         with requests.get(url, stream=True) as r:
-#             r.raise_for_status()
-#             with open(local_filename, 'wb') as f:
-#                 for chunk in r.iter_content(chunk_size=8192): 
-#                     # If you have chunk encoded response uncomment if
-#                     # and set chunk_size parameter to None.
-#                     #if chunk: 
-#                     f.write(chunk)
-#         return local_filename
+    def download_file(url, local_filename):
+        # NOTE the stream=True parameter below
+        with requests.get(url, stream=True, headers={'User-Agent': 'Bot of User:' + s.username()}) as r:
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        return local_filename
     
-#     s = Site()
-#     gen = GeneratorFactory(s)
-#     gen.handle_args(['-category:Character sprites', '-ns:File', '-titleregex:00'])
-#     gen = gen.getCombinedGenerator(preload=True)
-#     for file in gen:
-#         file: FilePage
-#         url = file.get_file_url()
-#         download_file(url)
+    gen = GeneratorFactory(s)
+    gen.handle_args(['-category:Character sprites', '-ns:File', '-titleregex:00|01'])
+    gen = gen.getCombinedGenerator(preload=False)
+    existing: set[str] = set()
+    for file in gen:
+        file: FilePage
+        title = file.title(with_ns=False).replace(" ", "_")
+        char_name = re.sub(r"_\d\d\.png$", "", title)
+        if char_name in existing:
+            continue
+        existing.add(char_name)
+        local_path = source_path / char_name / title
+        if local_path.exists():
+            continue
+        local_path.parent.mkdir(exist_ok=True)
+        url = file.get_file_url()
+        download_file(url, local_path)
+        print(char_name, "downloaded")
 
 
 result_file = Path("cache/seg-result.json")
@@ -50,11 +59,13 @@ def predict():
     TEXT_PROMPT = "face"
     BOX_TRESHOLD = 0.25
     TEXT_TRESHOLD = 0.25
-    
-    result: dict[str, list[float]] = {}
+
+    result: dict[str, list[float]] = json.load(open(result_file, "r", encoding="utf-8"))
 
     for character_path in source_path.iterdir():
         character_name = character_path.name
+        if character_name in result:
+            continue
         image_path = character_path.joinpath(character_name + "_00.png")
         if not image_path.exists():
             image_path = character_path.joinpath(character_name + "_01.png")
@@ -76,8 +87,8 @@ def predict():
         # print("|" + file_name + f"={width},{height},{center_x},{center_y}")
         result[character_name] =[width, height, center_x, center_y]
     
-    with open(result_file, "w") as f:
-        json.dump(result, f)
+    with open(result_file, "w", encoding='utf-8') as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
         
 # def process():
 #     result: dict = json.load(open(result_file, "r"))
@@ -87,38 +98,42 @@ def predict():
 #             print(f"|{k}={v[i]}")
 #         print("}}")
         
-# def process2():
-#     result: dict = json.load(open(result_file, "r"))
-#     multiplier = 4.2
-#     for k, v in result.items():
-#         k: str
-#         width, height, x, y = v
-#         x = x.item()
-#         y = y.item()
-#         style = f".story-image-{k.replace('_00.png', '').replace('(', '_').replace(')', '_')} {{ " \
-#                 f"margin-left: -{round(max(0, width / multiplier * x - 29), 2)}px; " \
-#                 f"margin-top: -{round(max(0, height / multiplier * y - 29), 2)}px " \
-#                 "}"
-#         print(style)
+def make_css():
+    result: dict = json.load(open(result_file, "r"))
+    multiplier = 4.2
+    for k, v in sorted(result.items()):
+        k: str
+        width, height, x, y = v
+        k, _ = re.subn(r'[＊() ,]', '_', k)
+        style = f".story-image-{k} img {{ " \
+                f"margin-left: -{round(max(0, width / multiplier * x - 29), 2)}px; " \
+                f"margin-top: -{round(max(0, height / multiplier * y - 29), 2)}px " \
+                "}"
+        print(style)
 
-filter = {}
 
 def crop_batch():
     result: dict = json.load(open(result_file, "r"))
+    out_path = Path('cache/cropped')
+    out_path.mkdir(exist_ok=True)
     manual: dict = {}
     for k, v in manual.items():
         result[k] = v
     portrait_width, portrait_height = 300, 300
     for k, v in result.items():
-        if len(filter) > 0:
-            if k not in filter:
-                continue
-            print(f"Processing {k}")
+        print(f"Processing {k}")
         width, height, center_x, center_y = v
         center_x, center_y = width * center_x, height * center_y
         top_left = [int(round(x)) for x in (center_x - portrait_width / 2, center_y - portrait_height / 2)]
         top_left = [f"+{x}" if x >= 0 else f"{x}" for x in top_left]
-        print(f'mogrify -crop {portrait_width}x{portrait_height}{top_left[0]}{top_left[1]} "{k}/{k}_??.png"')
+        source = source_path / k / f'{k}_00.png'
+        if not source.exists():
+            source = source_path / k / f'{k}_01.png'
+        subprocess.run(['magick',
+                        source,
+                        '-crop',
+                        f'{portrait_width}x{portrait_height}{top_left[0]}{top_left[1]}',
+                        out_path / f"{k}.png"])
 
 def merge_batch():
     out_path = Path("./sprites")
@@ -126,11 +141,8 @@ def merge_batch():
     sprite_path = Path("../ba-spinecharacters2")
     for character_path in sprite_path.iterdir():
         char_name = character_path.name
-        if len(filter) > 0:
-            if char_name not in filter:
-                continue
-            print(f"Processing {char_name}")
+        print(f"Processing {char_name}")
         files = [str(p) for p in character_path.glob("*.png")]
         _ = subprocess.run(['magick', *files, '+append', f"{out_path}/{char_name}_Sprite_Sheet.png"], shell=True)
         
-merge_batch()
+crop_batch()
